9be95b7594df8ddb209a8de18e058b88c328eb0c
Ederton Lima <contato@ederton.com.br>
2016-07-26 11:29:37 -0300

    Finalização da Home

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,27 @@
-<footer class="footer" id="contato">
-	<div class="container">
-
+<footer class="footer">
+	<div class="detalhe">
+		<div class="container">
+				<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4"></div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2 copy">
+					<a href="#"><img src="assets/images/lelis.png" alt="Lelis & Arruda"></a>
+				</div>
+				<div class="col-md-8 endereco">
+					<strong>R. N. S. Auxiliadora, 488 - Jd. Americano - 14 3532.3859</strong>
+					2016. Todos os direitos reservados. As marcas veiculadas ness site são de propriedade de suas respectivas empresas.
+				</div>
+				<div class="col-md-2 orpit">
+					Site produzido por <a href="#"><img src="assets/images/orpit.png" alt="Lelis & Arruda"></a>
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <?php include 'includes/header.php'; ?>
 
-<session class="slide-home">
+<section class="slide-home">
 	<div class="controle-slide">
 		<a class="left" href="#slide" role="button" data-slide="prev"></a>
 		<a class="right" href="#slide" role="button" data-slide="next"></a>
@@ -69,6 +69,173 @@
 			</div>
 		</div>
 	</div>
-</session>
+</section>
+
+<div class="content">
+	<div class="container">
+
+		<section class="obras-emandamento">
+			<h2><span class="icon-title"><img src="assets/images/icon-obras-andamento.png" alt=""></span>OBRAS EM ANDAMENTO</h2>
+			<div class="slide-obras">
+				<div class="controle-slide">
+					<span class="left prev" href="javascript:"></span>
+					<span class="right next" href="javascript:"></span>
+				</div>
+				<div class="carousel" id="obras-emandamento">
+					<div class="item active">
+						<div style="background-image: url('assets/images/img-obras-1.jpg');">
+							<span class="new">NOVO</span>
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-2.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-3.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-4.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-5.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-6.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="obras-concluidas">
+			<h2><span class="icon-title"><img src="assets/images/icon-obras-concluidas.png" alt=""></span>OBRAS CONCLUÍDAS</h2>
+			<div class="slide-obras">
+				<div class="controle-slide">
+					<span class="left prev" href="javascript:"></span>
+					<span class="right next" href="javascript:"></span>
+				</div>
+				<div class="carousel" id="obras-concluidas">
+					<div class="item active">
+						<div style="background-image: url('assets/images/img-obras-6.jpg');">
+							<span class="new">NOVO</span>
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-5.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-4.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-3.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-2.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+					<div class="item">
+						<div style="background-image: url('assets/images/img-obras-1.jpg');">
+							<span class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+							<a href="#">lorem ipsum dolor sit</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="contato-home">
+			<div class="row">
+				<div class="col-md-5 col-md-offset-1 contato">
+					<h2>CONTATO</h2>
+					<form action="">
+						<fieldset class="col-md-12">
+							<label for="nome">NOME/RAZÃO SOCIAL</label>
+							<input type="text" name="nome" id="nome">
+						</fieldset>
+						<fieldset class="col-md-12">
+							<label for="email">EMAIL</label>
+							<input type="text" name="email" id="email">
+						</fieldset>
+						<fieldset class="col-md-12">
+							<label for="telefone">TELEFONE</label>
+							<input type="text" name="telefone" id="telefone">
+						</fieldset>
+						<fieldset class="col-md-12">
+							<label for="mensagem">MENSAGEM</label>
+							<textarea name="mensagem" id="mensagem"></textarea>
+						</fieldset>
+						<fieldset class="col-md-12">
+							<button type="submit">ENVIAR</button>
+						</fieldset>
+					</form>
+				</div>
+				<div class="col-md-5 localizacao">
+					<h2>LOCALIZAÇÃO</h2>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3707.8540650733694!2d-49.76239564965556!3d-21.669533600268768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94be1432332deae1%3A0x75a1675087d8eea8!2sR.+Nossa+Sra.+Auxiliadora%2C+488+-+Jardim+Americano%2C+Lins+-+SP%2C+16400-650!5e0!3m2!1spt-BR!2sbr!4v1469534958680" allowfullscreen></iframe>
+				</div>
+			</div>
+		</section>
+
+	</div>
+</div>
+
+<script type="text/javascript" src="assets/js/jquery.bxslider.js"></script>
+<script type="text/javascript">
+	jQuery(function() {
+		jQuery('#obras-emandamento').bxSlider({
+			minSlides: 1,
+			maxSlides: 3,
+			slideWidth: 360,
+			slideMargin: 10,
+			nextSelector: '.obras-emandamento .next',
+			prevSelector: '.obras-emandamento .prev',
+			pager: false,
+			auto: false,
+			autoStart: false,
+			responsive: true
+		});
+
+		jQuery('#obras-concluidas').bxSlider({
+			minSlides: 1,
+			maxSlides: 3,
+			slideWidth: 360,
+			slideMargin: 10,
+			nextSelector: '.obras-concluidas .next',
+			prevSelector: '.obras-concluidas .prev',
+			pager: false,
+			auto: false,
+			autoStart: false,
+			responsive: true
+		});
+	});
+</script>
 
 <?php include 'includes/footer.php'; ?>
